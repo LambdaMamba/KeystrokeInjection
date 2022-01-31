f4b34f68c0afa662c39e2e257edd1ee3b50cf565
Lena <57588612+LambdaMamba@users.noreply.github.com>
2022-02-01 01:50:06 +0900

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+while true
+do
+    i=0
+    x=$((RANDOM%5))
+    randdirec=$((RANDOM%4))
+    dance=$((RANDOM%100))
+    g=$((RANDOM%100))
+    flag=0
+
+    if [ $dance -gt 95 ]
+    then
+        xdotool key z
+        sleep 2
+    fi
+
+    if [ $g -gt 95 ]
+    then
+        xdotool keydown g
+        flag=1
+    fi
+
+    while [ $i -lt $x ] 
+    do
+            case $randdirec in
+                0)
+                    xdotool key w
+                ;;
+
+                1)
+                    xdotool key s
+                ;;
+
+                2)
+                    xdotool key a
+                ;;
+
+
+                3)
+                    xdotool key d
+                ;;
+
+                *)
+                ;;
+            esac
+
+
+            sleep 0.05
+            i=$((i+1))
+    done
+    if [ $flag == 1 ]
+    then
+        xdotool keyup g
+        flag=0
+    fi
+
+done
